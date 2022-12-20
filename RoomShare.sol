fe5b2278508877200c8ef4d5b7389a01c87a534e
이호준 <2015019325@hanyang.ac.kr>
2022-12-21 00:17:23 +0900

    add override

--- RoomShare.sol
+++ RoomShare.sol
@@ -12,19 +12,19 @@ contract RoomShare is IRoomShare {
   mapping(address => Rent[]) public rentDB_renter; // 대여자의 대여 히스토리
 
 
-  function getMyRents() external view returns(Rent[] memory) {
+  function getMyRents() external view override returns(Rent[] memory) {
     /* 함수를 호출한 유저의 대여 목록을 가져온다. */
     return rentDB_renter[msg.sender];
   }
 
-  function getRoomRentHistory(uint _roomId) external view  returns(Rent[] memory) {
+  function getRoomRentHistory(uint _roomId) external view override returns(Rent[] memory) {
     /* 특정 방의 대여 히스토리를 보여준다. */
     return rentDB_room[_roomId];
   }
 
   function shareRoom( string calldata name, 
                       string calldata location, 
-                      uint price ) external  {
+                      uint price ) external override {
     /**
      * 1. isActive 초기값은 true로 활성화, 함수를 호출한 유저가 방의 소유자이며, 365 크기의 boolean 배열을 생성하여 방 객체를 만든다.
      * 2. 방의 id와 방 객체를 매핑한다.
@@ -35,7 +35,7 @@ contract RoomShare is IRoomShare {
     emit NewRoom(roomId++);
   }
 
-  function rentRoom(uint _roomId, uint year, uint checkInDate, uint checkOutDate) payable external {
+  function rentRoom(uint _roomId, uint year, uint checkInDate, uint checkOutDate) payable external override{
     /**
      * 1. roomId에 해당하는 방을 조회하여 아래와 같은 조건을 만족하는지 체크한다.
      *    a. 현재 활성화(isActive) 되어 있는지
@@ -77,7 +77,7 @@ contract RoomShare is IRoomShare {
       payable(owner).transfer(value);
   }
 
-  function recommendDate(uint _roomId, uint checkInDate, uint checkOutDate) external view returns(uint[2] memory) {
+  function recommendDate(uint _roomId, uint checkInDate, uint checkOutDate) external view override returns(uint[2] memory) {
     /**
      * 대여가 이미 진행되어 해당 날짜에 대여가 불가능 할 경우, 
      * 기존에 예약된 날짜가 언제부터 언제까지인지 반환한다.
@@ -105,7 +105,7 @@ contract RoomShare is IRoomShare {
   // optional 1
   // caution: 방의 소유자를 먼저 체크해야한다.
   // isActive 필드만 변경한다.
-  function markRoomAsInactive(uint256 _roomId) external {
+  function markRoomAsInactive(uint256 _roomId) external override {
     Room storage room = roomDB[_roomId];
     // 방의 소유자 체크
     require(room.owner == msg.sender, "Not owner of room");
@@ -115,7 +115,7 @@ contract RoomShare is IRoomShare {
     // optional 2
     // caution: 변수의 저장공간에 유의한다.
     // isRented 필드의 초기화를 진행한다. 
-  function initializeRoomShare(uint _roomId) external {
+  function initializeRoomShare(uint _roomId) external override {
     Room storage room = roomDB[_roomId];
     // 방의 소유자 체크
     require(room.owner == msg.sender, "Not owner of room");
